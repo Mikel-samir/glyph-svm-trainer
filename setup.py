6e3f63739a0b77ff3b0f53dc0408f49302b97405
Michael Samir <michaelsamir1999@gmail.com>
2021-01-30 18:43:43 +0200

    init setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(name='svm-trainer',
+      version='0.1.0',
+      description='svm training utils .',
+      author='Michael Samir',
+      author_email='michaelsamir1999@gmail.com',
+      url='https://github.com/Mikel-samir/svm-trainer',
+      py_modules=['lazyImage','lazyDataset','lazyModel'],
+      license='GPLv3',
+#      requires=['numpy','pandas','imageio'],
+     )
